720bf8cf6846a637dcfb8d60b4ded2d77a3009da
carlosAlbertoGlobal <carlos.alberto@globalfinanceiro.com.br>
2023-06-29 15:05:03 -0300

    feat: Customizando status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de curso';
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de curso');
   }
   @Get(':id')
   findOne(@Param() params) {
     return `Curso #${params.id}`;
+  }
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
   }
 }
